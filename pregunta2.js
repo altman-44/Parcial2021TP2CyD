02f1c5b52b4124bf6099c1b225a6ef8aa0a569c5
altman-44 <altman.nicolas44@gmail.com>
2021-04-14 21:06:51 -0300

    Fixed sth in pregunta2

--- pregunta2.js
+++ pregunta2.js
@@ -15,13 +15,13 @@
 }
 
 const sale = function (article, cant){
-if (inventory[article] != undefined) {
-  if (inventory[article] >= cant) {
-    inventory[article] -= cant
-    return 200
+  if (inventory[article] != undefined) {
+    if (inventory[article] >= cant) {
+      inventory[article] -= cant
+      return 200
+    }
   }
-}
-return 500
+  return 500
 }
 
 // TESTS (no modificar)
